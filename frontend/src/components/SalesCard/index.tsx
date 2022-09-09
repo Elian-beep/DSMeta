9aedad8de26a1493726ae4e780a90eff12b00eb9
Elian-beep <elianoliver101@gmail.com>
2022-09-09 16:55:14 -0300

    Atualização dos dados por data

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -20,12 +20,16 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)
+
+        const dMin = minDate.toISOString().slice(0, 10);
+        const dMax = maxDate.toISOString().slice(0, 10);
+        
+        axios.get(`${BASE_URL}/sales?minDate=${dMin}&maxDate=${dMax}`)
             .then(response => { //O response é a resposta válida recebido da requisição
                 setSales(response.data.content);
 
             })
-    }, []);
+    }, [minDate, maxDate]);
 
     return (
         <div className="dsmeta-card">
